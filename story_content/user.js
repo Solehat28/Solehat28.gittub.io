eb8b9c1cbd3784628d4cbe8914ac140fa71c1889
Solehat28 <2224220006@untirta.ac.id>
2024-06-27 09:20:58 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,20 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6V5zF8LjH8p":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementByld('bgSongku')
+audio.src='musikku.mp3*;
+audio.load();
+audio.play();
+audio.volume=0.3;
+
+}
+
